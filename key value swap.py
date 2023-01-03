fddc87eaca232d579ed330b32fa24bf867ada031
Harjeet <harjeetsinghkhaira745@gmail.com>
2023-01-03 12:45:37 +0530

    code changed

--- key value swap.py
+++ key value swap.py
@@ -1,3 +1,13 @@
 a={1:'one',2:'one'}
-b=dict(zip(a.values(),a.keys()))
-print(b)
+b=a.keys()
+c=a.values()
+z='y'
+if list(c)!=list(set(c)):
+    print('values are repeating')
+    z=input('do you still want to print (y/n): ')
+if z=='y' or z=='Y':
+    print(dict(zip(c,b)))
+elif z=='n' or z=='N':
+    print('whatever you want...')
+else:
+    print('Invalid input')
